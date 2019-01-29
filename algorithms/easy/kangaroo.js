81177c752d6be735b693f7511e6a243691280b3b
alyssa0528 <alyssa.kim@gmail.com>
2019-01-29 11:46:10 +0000

    add kangaroo solution

---
+++ algorithms/easy/kangaroo.js
@@ -0,0 +1,64 @@
+// You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+
+// The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+// The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+// You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the kangaroo function below.
+function kangaroo(x1, v1, x2, v2) {
+    let answer = "NO";
+    if (v2 > v1) {
+        return answer;
+    } else if (((x2 - x1) % (v2 - v1)) === 0) {
+        answer = "YES";
+    } else {
+        answer = "NO";
+    }
+    return answer;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const x1V1X2V2 = readLine().split(' ');
+
+    const x1 = parseInt(x1V1X2V2[0], 10);
+
+    const v1 = parseInt(x1V1X2V2[1], 10);
+
+    const x2 = parseInt(x1V1X2V2[2], 10);
+
+    const v2 = parseInt(x1V1X2V2[3], 10);
+
+    let result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
