5a12f4e42c85029556412c13002694cd02ce00b7
alyssa0528 <alyssa.kim@gmail.com>
2019-02-19 12:03:16 +0000

    add solution for birthdayChocolate

---
+++ algorithms/easy/birthdayChocolate.js
@@ -0,0 +1,65 @@
+// Lily has a chocolate bar that she wants to share it with Ron for his birthday. Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected such that the length of the segment matches Ron's birth month and the sum of the integers on the squares is equal to his birth day. You must determine how many ways she can divide the chocolate.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+    let choc = [];
+    if (m > s.length) {
+        return 0;
+    } else {
+        for (let i = 0; i <= (s.length - m); i++) {
+            let subArray = s.slice(i, i + m);
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            let sum = subArray.reduce(reducer);
+            if (sum === d) {
+                choc.push(sum)
+            }
+        }
+    }
+    console.log(choc)
+    return choc.length;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d = parseInt(dm[0], 10);
+
+    const m = parseInt(dm[1], 10);
+
+    const result = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
