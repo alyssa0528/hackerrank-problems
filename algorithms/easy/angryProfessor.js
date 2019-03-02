aefd3d36137d20edad2b309283ec33d77e4874e0
alyssa0528 <alyssa.kim@gmail.com>
2019-03-02 09:46:32 +0000

    add solution for angryProfessor

---
+++ algorithms/easy/angryProfessor.js
@@ -0,0 +1,59 @@
+// A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, he decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time (arrivalTime <= 0) to arrived late (arrivalTime > 0).
+
+// Given the arrival time of each student and a threshhold number of attendees, determine if the class is canceled.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the angryProfessor function below.
+function angryProfessor(k, a) {
+    const onTime = a.filter(time => time <= 0);
+
+    return onTime.length >= k ? "NO" : "YES";
+}
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const nk = readLine().split(' ');
+
+        const n = parseInt(nk[0], 10);
+
+        const k = parseInt(nk[1], 10);
+
+        const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+        let result = angryProfessor(k, a);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
