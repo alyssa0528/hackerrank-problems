7019ee918d31e58e781686886e914fc8ff59ac20
alyssa0528 <alyssa.kim@gmail.com>
2019-03-09 16:09:47 +0000

    add solution for repeatedString

---
+++ algorithms/easy/repeatedString.js
@@ -0,0 +1,52 @@
+Lilah has a string, s, of lowercase English letters that she repeated infinitely many times.
+
+Given an integer, n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+
+For example, if the string s = 'abcac' and n = 10, the substring we consider is abcacabcac, the first 10 characters of her infinite string. There are 4 occurrences of a in the substring.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    let count = (s.split('').filter(letter => letter === "a").length) * Math.floor(n / s.length);
+    let remaining = s.slice(0, n % s.length).split('').filter(letter => letter === "a").length;
+    return count + remaining;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
